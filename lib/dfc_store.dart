7384d767779641a4e7d4c6701a50711f72f7b9fd
Steve Gehrman <sgehrman@gmail.com>
2025-05-19 19:26:41 -0700

    sdf

--- lib/dfc_store.dart
+++ lib/dfc_store.dart
@@ -1,5 +1,7 @@
-export './dfc_store_models.dart';
 export 'src/license_key_manager.dart';
+export 'src/models/license_key_model.dart';
+export 'src/models/license_response_model.dart';
+export 'src/models/registered_domain_model.dart';
 export 'src/server_rest_api.dart';
 export 'src/store_utils.dart';
 export 'src/widgets/lost_license_form.dart';
--- lib/dfc_store_models.dart
+++
@@ -1,3 +0,0 @@
-export 'src/models/license_key_model.dart';
-export 'src/models/license_response_model.dart';
-export 'src/models/registered_domain_model.dart';
