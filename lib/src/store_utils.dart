3ea113e96370b2919d22de88a9dc03bc69d01e5b
Steve Gehrman <sgehrman@gmail.com>
2025-07-08 17:10:44 -0700

    sdf

--- lib/src/store_utils.dart
+++ lib/src/store_utils.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/services.dart';
 
 class StoreUtils {
+  StoreUtils._();
+
+  static bool showStylingTools = false;
+
   // The websites have a hidden method of bringing up the admin panel
   static bool get isAdminKeysPressed =>
       HardwareKeyboard.instance.logicalKeysPressed
           .containsAll(<LogicalKeyboardKey>[
         LogicalKeyboardKey.shiftLeft,
+        LogicalKeyboardKey.altLeft,
+      ]);
+
+  static bool get isStyleKeysPressed =>
+      HardwareKeyboard.instance.logicalKeysPressed
+          .containsAll(<LogicalKeyboardKey>[
+        LogicalKeyboardKey.escape,
         LogicalKeyboardKey.altLeft,
       ]);
 }
